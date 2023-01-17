bbdcb245b0c89e8e929d48635d60789eb63335f7
SamBasch <baschnagelsam@gmail.com>
2023-01-17 16:42:07 -0500

    mylogo and codepage

--- js/site.js
+++ js/site.js
@@ -1,12 +1,113 @@
-function displayMessage() {
-    let msg = document.getElementById('message').value;
+//get the user input from the page
 
-    Swal.fire(
-        {
-            backdrop: false,
-            title: 'App Name',
-            text: msg
-        }
-    );
+    //controller function or entry point function (kicks off the whole process)
+function fetchUserInput() {
+    //get values from page
+
+
+    let startValue = document.getElementById('startValue').value;
+
+    let endValue = document.getElementById('endValue').value;
+
+
+
+
+    //parse our inputs as numbers
+
+    startValue = parseInt(startValue);
+    endValue = parseInt(endValue);
+
+
+
+    //verify our inputs are actually numbers (validation)
+
+    if (Number.isInteger(startValue) && Number.isInteger(endValue)) {
+
+            let numbersArray = generateNumbers(startValue, endValue);
+
+            displayNumbers(numbersArray);
+
+
+    } else {
+
+        Swal.fire(
+            {
+                icon: 'error',
+                title: 'Oops!',
+                text: 'Only integers are allowed for Range Finder!'
+
+            }
+        );
+
+
+    }
+
+    //after validation generate our numbrs
+    //display them on page
+
+
+        
+}
+
+    
+
+
+
+//generate our numbers
+
+function generateNumbers(start, end) {
+
+    let numbers = [];
+
+    for(let i = start; i <= end; i++) {
+
+        numbers.push(i);
+    }
+
+    return numbers;
+
+
 
 }
+
+
+
+//display them on page
+
+//view funciton (function is dedicated to making it 'show up' on our web page)
+function displayNumbers(numbersArray) {
+        let tableBody = document.getElementById('results');
+
+
+
+        let tableHtml = "";
+
+        for(let i = 0; i < numbersArray.length; i++) {
+            
+            let value = numbersArray[i];
+            let className = '';
+
+            if (value % 2 == 0) {
+                className = 'even'
+            } else {
+                className = 'odd'
+            }
+
+
+            if(i % 5 == 0) {
+                tableHtml = tableHtml + '<tr>'
+            }
+
+            let tableRow = `<td class="${className}">${value}</td>`
+
+            tableHtml = tableHtml + tableRow;
+
+            if (i + 1 % 5 == 0) {
+                tableHtml = tableHtml + '</tr>'
+            }
+            
+        }
+        
+        tableBody.innerHTML = tableHtml;
+
+    }
